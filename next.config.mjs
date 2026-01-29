fc2c35724629371de93823bd38a9efdc1f4078a2
Ryan Blackman <rblackman@mtd.org>
2026-01-29 13:47:47 -0600

    style: fixed formatting

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import nextMDX from "@next/mdx";
 import { withSentryConfig } from "@sentry/nextjs";
 import createWithVercelToolbar from "@vercel/toolbar/plugins/next";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
