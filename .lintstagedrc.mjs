c3719b06382704793c68e35d1aab04529b6d3091
Ryan Blackman <rblackman24@gmail.com>
2026-02-01 08:57:06 -0600

    chore: add some debugging to lint staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,12 +1,24 @@
 // ./.lintstagedrc.mjs
 import path from "node:path";
 
+/**
+ * @param {string} p
+ * @returns {string}
+ */
 const quote = (p) => `"${p}"`;
 
+/**
+ * @param {string[]} filenames
+ * @returns {string[]}
+ */
 const toRepoRelative = (filenames) => {
 	return filenames.map((f) => path.relative(process.cwd(), f)).map((f) => f.split(path.sep).join("/"));
 };
 
+/**
+ * @param {string} f
+ * @returns {boolean}
+ */
 const isIgnored = (f) => {
 	return (
 		f.includes("/.next/") ||
@@ -17,21 +29,31 @@ const isIgnored = (f) => {
 	);
 };
 
+/**
+ * @param {string[]} filenames
+ * @returns {string}
+ */
 const biomeCheck = (filenames) => {
-	const files = toRepoRelative(filenames)
-		.filter((f) => {
-			return !isIgnored(f);
-		})
-		.map(quote)
-		.join(" ");
+	const filesArray = toRepoRelative(filenames).filter((f) => {
+		return !isIgnored(f);
+	});
+
+	console.log("Biome checking files:", filesArray);
+
+	const files = filesArray.map(quote).join(" ");
 
 	if (!files) {
 		return 'node -e "process.exit(0)"';
 	}
 
-	return `pnpm exec biome check --write --no-errors-on-unmatched --files-ignore-unknown=true --colors=off ${files}`;
+	return `pnpm -w exec biome check --write --no-errors-on-unmatched --files-ignore-unknown=true --colors=off .`;
 };
 
+/**
+ * @param {string[]} filenames
+ * @param {string} prefix
+ * @returns {boolean}
+ */
 function stagedFilesUnder(filenames, prefix) {
 	return (
 		toRepoRelative(filenames).filter((f) => {
