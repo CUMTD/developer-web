cf3fdf159fe66a7e48c3e67abb455d8ef13efac7
Ryan Blackman <rblackman@mtd.org>
2026-02-02 12:25:03 -0600

    style: cleanup

--- packages/client/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { paths } from "@mtd/developer-api-types";
-export type MtdApiClientOptions = {
-    baseUrl: string;
-    apiKey: string;
-    defaultHeaders?: Record<string, string>;
-};
-export declare const createMtdApiClient: ({ baseUrl, apiKey, defaultHeaders }: MtdApiClientOptions) => import("openapi-fetch").Client<paths, `${string}/${string}`>;
