c5900b47ab9e433161c7875d873aa57bfea9415d
Ryan Blackman <rblackman@mtd.org>
2026-03-05 14:09:45 -0600

    chore: format

--- site/app/reference/[slug]/opengraph-image.tsx
+++ site/app/reference/[slug]/opengraph-image.tsx
@@ -4,11 +4,5 @@ import type { Props } from "./page";
 export default async function Image({ params }: Props) {
 	const { slug } = await params;
 
-	return (
-		OpenGraphImage({ route: `/${slug}` }),
-		{
-			width: 1200,
-			height: 630,
-		}
-	);
+	return OpenGraphImage({ route: `/${slug}` });
 }
