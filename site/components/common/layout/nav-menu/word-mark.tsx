1b1712ce2b3592e8cc2689de4d3b1e1ed30b2126
Ryan Blackman <rblackman@mtd.org>
2026-03-06 12:00:27 -0600

    feature: mono in terminal

--- site/components/common/layout/nav-menu/word-mark.tsx
+++ site/components/common/layout/nav-menu/word-mark.tsx
@@ -6,7 +6,7 @@ export default function WordMark() {
 	return (
 		<Link href={"/"} className="flex flex-row items-center gap-2">
 			<DevLogo width={60} />
-			<span className="text-2xl italic font-bold hidden md:block font-mono">Developer Resources</span>
+			<span className="text-2xl font-bold hidden md:block font-mono">Developer Resources</span>
 			<Cursor />
 		</Link>
 	);
